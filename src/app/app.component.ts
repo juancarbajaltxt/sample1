85d9f5b3e4481bbead34782dbcb553f77bc24e7e
TXT-52 <txt-52@MacBook-Pro-5.lan1>
2020-06-28 18:01:13 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+// import { bloomHasToken } from '@angular/core/src/render3/di';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angulartask';
+
+  title = 'angular-first-task';
+  public form: FormGroup;
+  public blogs =[];
+
+constructor(public _formBuilder: FormBuilder) {
+  // group() constructs a new FormGroup instance
+  this.form = this._formBuilder.group(
+    {
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+    }
+  );
+}
+
+onSubmit() {
+  console.log(this.form.value);
+  
+  this.blogs.push(this.form.value)
+
+  
+}
+
 }
